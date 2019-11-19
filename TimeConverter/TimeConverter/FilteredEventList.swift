5b0af5122e787aaa426b26a2f706d993de5f00f6
bgayman <brad.gayman@wework.com>
2019-11-18 21:40:13 -0500

    Add FilterListView

---
+++ TimeConverter/TimeConverter/FilteredEventList.swift
@@ -0,0 +1,21 @@
+//
+//  FilteredEventList.swift
+//  TimeConverter
+//
+//  Created by brad.gayman on 11/18/19.
+//  Copyright © 2019 bumnetworks. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilteredEventList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteredEventList_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredEventList()
+    }
+}
